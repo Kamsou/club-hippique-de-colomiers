6f5f4eec15f8f9dfc56afe4be8df36b80ffd75fb
Kamsou <camille.coutens@gmail.com>
2025-09-01 10:17:18 +0200

    refactor: remove unused server functions and related files
    - Deleted nitro source map and main server files.
    - Removed contact API handler and its source map.
    - Cleaned up renderer and SPA template files.
    - Updated netlify.toml to change publish directory from .output/public to dist.

--- .netlify/functions-internal/server/chunks/build/index-styles-1.mjs-p4NpHCKk.mjs
+++
@@ -1,4 +0,0 @@
-const index_vue_vue_type_style_index_0_scoped_b8dcab4b_lang = ".text-primary[data-v-b8dcab4b]{color:#0073a8}@keyframes fade-in-up-b8dcab4b{0%{opacity:0;transform:translateY(20px)}to{opacity:1;transform:translateY(0)}}.animate-fade-in-up[data-v-b8dcab4b]{animation:fade-in-up-b8dcab4b .8s ease-out forwards}";
-
-export { index_vue_vue_type_style_index_0_scoped_b8dcab4b_lang as i };
-//# sourceMappingURL=index-styles-1.mjs-p4NpHCKk.mjs.map
--- .netlify/functions-internal/server/chunks/build/index-styles.DO-4noyK.mjs
+++
@@ -1,6 +0,0 @@
-import { i as index_vue_vue_type_style_index_0_scoped_b8dcab4b_lang } from './index-styles-1.mjs-p4NpHCKk.mjs';
-
-const indexStyles_DO4noyK = [index_vue_vue_type_style_index_0_scoped_b8dcab4b_lang, index_vue_vue_type_style_index_0_scoped_b8dcab4b_lang];
-
-export { indexStyles_DO4noyK as default };
-//# sourceMappingURL=index-styles.DO-4noyK.mjs.map
