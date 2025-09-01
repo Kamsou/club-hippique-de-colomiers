dc2f5e37fe5f90c34e7b7977b28ffd4d7760c82c
Kamsou <camille.coutens@gmail.com>
2025-09-01 10:09:36 +0200

    feat: add contact form API with email sending functionality using Nodemailer
    - Implemented a new API endpoint for handling contact form submissions.
    - Validated input fields and sanitized user input to prevent XSS attacks.
    - Configured Nodemailer to send emails via Gmail.
    - Added error handling for email sending process.
    - Created necessary source maps for debugging.
    
    chore: set up server structure and dependencies
    
    - Added server entry point and configuration for Netlify functions.
    - Included required dependencies in package.json for server functionality.
    - Updated Netlify configuration to use Node.js version 20 and include server files.

---
+++ .netlify/functions-internal/server/chunks/build/index-styles.CYt47sjl.mjs
@@ -0,0 +1,6 @@
+import { i as index_vue_vue_type_style_index_0_scoped_b8dcab4b_lang } from './index-styles-1.mjs-p4NpHCKk.mjs';
+
+const indexStyles_CYt47sjl = [index_vue_vue_type_style_index_0_scoped_b8dcab4b_lang];
+
+export { indexStyles_CYt47sjl as default };
+//# sourceMappingURL=index-styles.CYt47sjl.mjs.map
